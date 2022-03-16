fa1a07401523f1c15b2aa993e3ad516411908961
Leo-paul <leopaulmartin@hotmail.fr>
2022-03-16 23:10:51 +0100

    one collection card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-import Header from './components/Header';
+import "./App.css";
+import CollectionCard from "./components/CollectionCard";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
-      <Header/>
+      <Header />
+      <CollectionCard
+        id={0}
+        name={"Bandana Punk"}
+        traits={[{ value: 7 }]}
+        image="https://lh3.googleusercontent.com/9B1oTFEv4DeaW9gFwnd1ION2NzfGnC9lHfjnPNSbNkDZmID0P7Di6Y6ptZxZuf0oaWgnAKLu1eqEbdlCJSpHDFUceo2D7H9c0wK56Q=s0"
+      />
     </div>
   );
 }
---
+++ src/components/CollectionCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./CollectionCard.css";
+import weth from "../assets/weth.png";
+
+// testnet opensea https://testnets.opensea.io/0x69f24f7e3c183605dd4b3205d4623ef4df2ce9ed
+const CollectionCard = ({ id, name, traits, image }) => {
+  return (
+    <div className="collectionCard">
+      <img src={image} alt="punkImage" />
+      <div className="details">
+        <div className="name">
+          {name} <div className="id">#{id}</div>
+        </div>
+        <div className="priceContainer">
+          <img src={weth} alt="weth" className="wethImage" />
+          <div className="price">{traits[0]?.value}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CollectionCard;
