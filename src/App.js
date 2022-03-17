de5997af3e707e1d24c42b8f14f4cb7db480b47b
Leo-paul <leopaulmartin@hotmail.fr>
2022-03-17 22:41:43 +0100

    collection punk with opensea-api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import "./App.css";
 import CollectionCard from "./components/CollectionCard";
 import Header from "./components/Header";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Punklist from "./components/Punklist";
 
 function App() {
+  const [punkListData, setPunkListData] = useState([]);
+
+  useEffect(() => {
+    // url sample: https://testnets-api.opensea.io/assets?order_direction=asc&asset_contract_address=%7B%7Byour_collection_address_here%7D%7D
+    // use project url blockchain address and not personal wallet address
+    const getMyNfts = async () => {
+      const openseaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?order_direction=asc&asset_contract_address=0x56e6B5dFd5447f23d8a192FcD304A9656CCdD1a6"
+      );
+      console.log(openseaData.data.assets);
+      setPunkListData(openseaData.data.assets);
+    };
+    return getMyNfts();
+  }, []);
+
   return (
     <div className="app">
       <Header />
-      <CollectionCard
-        id={0}
-        name={"Bandana Punk"}
-        traits={[{ value: 7 }]}
-        image="https://lh3.googleusercontent.com/9B1oTFEv4DeaW9gFwnd1ION2NzfGnC9lHfjnPNSbNkDZmID0P7Di6Y6ptZxZuf0oaWgnAKLu1eqEbdlCJSpHDFUceo2D7H9c0wK56Q=s0"
-      />
+      <Punklist punkListData={punkListData} />
     </div>
   );
 }
